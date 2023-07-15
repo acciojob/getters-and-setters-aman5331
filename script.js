2bee0c91336a6b627c8cede3f52b213ae4d97599
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-07-15 23:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 //complete this code
-class Person {}
+class Person {
+	 constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set age(newAge) {
+    this._age = newAge;
+  }
+
+  study() {
+    console.log(`${this._name} is studying.`);
+  }
+}
+
+class Student extends Person {
+	constructor(name, age) {
+    super(name, age);
+  }
+}
+
+class Teacher extends Person {
+	constructor(name, age) {
+    super(name, age);
+  }
+
+  teach() {
+    console.log(`${this._name} is teaching.`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
